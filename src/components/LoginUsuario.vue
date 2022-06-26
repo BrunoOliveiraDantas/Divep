bfa0da5d61859046b799f09191a4e6aaef616d7d
Caixeta79 <83028957+Caixeta79@users.noreply.github.com>
2022-06-26 07:29:39 -0300

    Correção na tela de login

--- src/components/LoginUsuario.vue
+++ src/components/LoginUsuario.vue
@@ -1,21 +1,33 @@
 <template>
 
-    <form class="form-signin">
+  <div class="container1 col-md-12"> 
+    <div class="col-md-3 mt-5"> 
+    <div class="row"> 
+      <div class="col-12"> 
       
-      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-      <label for="inputEmail" class="sr-only">Email address</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
-      <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+      
+      <h1 class="h3 mb-3 font-weight-normal">Login</h1>
+      <label for="inputEmail" class="sr-only">Email</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Informe o Email" required="" autofocus="">
+      <label for="inputPassword" class="sr-only">Senha</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Informe a Senha" required="">
       <div class="checkbox mb-3">
         <label>
-          <input type="checkbox" value="remember-me"> Remember me
+          <input type="checkbox" value="remember-me"> Lembre-me
         </label>
       </div>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      <p class="mt-5 mb-3 text-muted">© 2017-2018</p>
-    </form>
-  
-
-
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+      
+    
+      </div>
+    </div>
+  </div>
+  </div>
 </template>
+<style type="text/css">
+.container1{
+  display: flex;
+  justify-content: center;
+}
+
+</style>
