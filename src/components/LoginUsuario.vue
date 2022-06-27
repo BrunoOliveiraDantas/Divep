9a30ab13f351f8d4f3ab696c28eaa30f1698fade
Bruno <oliveiradantas96@gamil.com>
2022-06-26 21:31:05 -0300

    Implementando campos de fomulario

--- src/components/LoginUsuario.vue
+++ src/components/LoginUsuario.vue
@@ -1,88 +1,101 @@
 <template>
 
-  <div class="container1 col-md-12"> 
+  <div class="container1 col-md-12">
 
-    <div class="col-md-3 mt-5"> 
-    <div class="row"> 
-      <div class="col-12"> 
-      
-      
-      <h1 class="h3 mb-3 font-weight-normal" >Login</h1>
-      <label for="inputEmail" class="sr-only" >Matricula</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Informe a Matrícula" required="" autofocus="" v-model="matricula">
-      <label for="inputPassword" class="sr-only" >Senha</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Informe a Senha" required="" v-model="senha">
-      <div class="checkbox mb-3">
-        <label>
-          <input type="checkbox" value="remember-me"> Lembre-me
-        </label>
-         
-      </div>
-         <div id="alerta-mensagem" class="d-none alert alert-warning alert-dismissible fade show" role="alert">
+    <div class="card-login col-md-3 mt-5">
+      <div class="row">
+        <div class="col-12">
+
+
+          <h1 class="h3 mb-3 font-weight-normal">Login</h1>
+          <label for="inputEmail" class="sr-only">Matricula</label>
+          <input type="email" id="inputEmail" class="form-control" placeholder="Informe a Matrícula" required=""
+            autofocus="" v-model="matricula">
+          <label for="inputPassword" class="sr-only">Senha</label>
+          <input type="password" id="inputPassword" class="form-control" placeholder="Informe a Senha" required=""
+            v-model="senha">
+          <div class="checkbox mb-3">
+            <label>
+              <input type="checkbox" value="remember-me"> Lembre-me
+            </label>
+
+          </div>
+          <div id="alerta-mensagem" class="d-none alert alert-warning alert-dismissible fade show" role="alert">
             <strong>Falha de Login!</strong> Usuário ou senha estão incorretos.
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
           </div>
-      <button class="btn btn-lg btn-primary btn-block" type="button" @click="validar()">Entrar</button>
-    
-    
+          <button class="btn btn-lg btn-primary btn-block" type="button" @click="validar()">Entrar</button>
+
+
+        </div>
       </div>
     </div>
   </div>
-  </div>
 </template>
 
-<script> 
+<script>
 
 export default {
 
-data(){
-  return {
+  data() {
+    return {
 
-    matricula:"",
-    senha:"",
+      matricula: "",
+      senha: "",
+
+    }
+  },
+  computed: {
+
+  },
+  component: {
+
+  },
+  moutend() {
+
+  },
+  methods: {
+
+    validar() {
+
+      if (this.matricula == "123" && this.senha == "123") {
+
+        var parametros = {
+
+        };
+        this.$router.push({
+          path: "helloWorld",
+          params: { parametros: parametros },
+        });
+
+      } else {
+        var alerta = document.getElementById('alerta-mensagem');
+        alerta.classList.toggle('d-none');
+      }
+
+    }
 
   }
-},
-computed: {
-
-},
-component:{
-
-},
-moutend(){
-
-},
-methods:{
-
-  validar(){
-
-if(this.matricula == "123" && this.senha == "123"){
-
-   var parametros = {
-              
-            };
-            this.$router.push({
-              path: "helloWorld",
-              params: { parametros: parametros },
-            });
-
-} else {
-  var alerta = document.getElementById('alerta-mensagem');
-  alerta.classList.toggle('d-none');
-}
-    
-  }
-
-}
 
 }
 </script>
 
 
 <style type="text/css">
-.container1{
+.container1 {
   display: flex;
   justify-content: center;
+  position: relative;
+  
+
 }
 
+/* Card */
+.card-login {
+  border: 1px solid #FFFFFF;
+  box-shadow: 0px 10px 30px rgba(0, 0, 0, 0.15);
+  border-radius: 4px;
+  padding: 1rem;
+  margin: 0 auto;
+}
 </style>
