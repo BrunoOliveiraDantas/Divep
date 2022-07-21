0e6dd34e694a6845ac4adbb8b2f204f95084e93b
Bruno <oliveiradantas96@gamil.com>
2022-07-21 16:09:53 -0300

    Serviços do Usuario.

--- src/service/usuarioService.js
+++ src/service/usuarioService.js
@@ -1,17 +1,46 @@
-
 import axios from "axios";
 
 export const usuarioService = {
-
-    
-    salvarUsuario(usuario) {
-        return new Promise((resolve, reject) => {
-          axios.post(`http://sigepi.adalberto1538.c41.integrator.host/usuario`, usuario)
-          .then(ret => {
-            resolve(ret)
-          }).catch(err => {
-            reject(err)
-          })
+  salvarUsuario(usuario) {
+    return new Promise((resolve, reject) => {
+      axios
+        .post(
+          `http://sigepi.adalberto1538.c41.integrator.host/usuario`,
+          usuario
+        )
+        .then((ret) => {
+          resolve(ret);
+        })
+        .catch((err) => {
+          reject(err);
         });
-      },
-}
+    });
+  },
+  listarUsuarioPorCpf(cpf) {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(`http://sigepi.adalberto1538.c41.integrator.host/usuario/${cpf}`)
+        .then((ret) => {
+          resolve(ret);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  },
+  editarUsuario(id, usuario) {
+    return new Promise((resolve, reject) => {
+      axios
+        .put(
+          `http://sigepi.adalberto1538.c41.integrator.host/usuario/value=/${id}`,
+          usuario
+        )
+        .then((ret) => {
+          resolve(ret);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  },
+};
